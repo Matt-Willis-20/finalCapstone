dce24d0fa5f34eacf2240cecacbbdc2a2008ed36
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2023-01-11 15:33:15 +0000

    first commit

---
+++ Scripts/comments.js
@@ -0,0 +1,38 @@
+// -------------------------------------------------------
+// Comments functionality
+// -------------------------------------------------------
+
+let comments = []
+
+const commentList = document.getElementById("comments");
+
+if (localStorage.getItem("comments") !== null) {
+    comments = JSON.parse(localStorage.getItem("comments"))
+    comments.reverse();
+    comments.forEach((item) => {
+        let text = document.createElement("p");
+        text.innerText = item.comment
+        text.className = "comment-text";
+        commentList.appendChild(text);
+    })
+}
+
+class Comment {
+    constructor(comment) {
+        this.comment = comment
+    }
+}
+
+function addComment() {
+    if (localStorage.getItem("comments") !== null) {
+        comments = JSON.parse(localStorage.getItem("comments"))
+        }
+    let newComment = new Comment(
+        document.getElementById("commentText").value
+    );
+    comments.push(newComment);
+    localStorage.setItem("comments", JSON.stringify(comments))
+}
+
+const commentBtn = document.getElementById("comment-btn");
+commentBtn.addEventListener("click", addComment);
